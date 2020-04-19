a18e4fb7435ae8c740a0798b364868f09f51ec3a
osmanakol <osmanakol@outlook.com>
2020-04-20 00:22:27 +0300

    master

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import { host,port } from "../host.json";
 import app from "./app";
 
- const port2:string|number = process.env.PORT || port;
+ const port2 = process.env.PORT || port;
 
 app.listen(port2,()=>{
     console.log(`Server is working, http://${host}:${port2}/`)
