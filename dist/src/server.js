ae10f1e898e79183aa971b9f1d3eb2d2d51903e3
osmanakol <osmanakol@outlook.com>
2020-04-20 13:28:16 +0300

    master

--- dist/src/server.js
+++ dist/src/server.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const host_json_1 = require("../host.json");
 const app_1 = __importDefault(require("./app"));
-const port2 = process.env.PORT || host_json_1.port;
+const port2 = process.env.PORT || 3003;
 app_1.default.listen(port2, () => {
     console.log(`Server is working, http://${host_json_1.host}:${port2}/`);
 });
