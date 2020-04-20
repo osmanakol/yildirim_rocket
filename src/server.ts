ddd00467a709879ba17d31554821a748a34ddc64
osmanakol <osmanakol@outlook.com>
2020-04-20 11:38:48 +0300

    mastere

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
-import { host,port } from "../host.json";
+import { host, port } from "../host.json";
 import app from "./app";
 
- const port2 = process.env.PORT || port;
-app.listen(port2,()=>{
+const port2 = process.env.PORT || port;
+app.listen(port2, () => {
     console.log(`Server is working, http://${host}:${port2}/`)
 });
